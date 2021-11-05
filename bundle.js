5c631e2bcf8a6e04de103a3c0456205a900a50b3
shane gibney <shanegibney@gmail.com>
2021-11-05 12:36:43 +0000

    first commit

---
+++ bundle.js
@@ -0,0 +1,7 @@
+(function () {
+	'use strict';
+
+	d3.select('body').text('iindex.js elloss');SS
+
+})();
+//# sourceMappingURL=bundle.js.map
---
+++ index.js
@@ -0,0 +1,2 @@
+import{ select} from 'd3';
+d3.select('body').text('iindex.js elloss');
